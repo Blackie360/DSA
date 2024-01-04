1dc8340c88b0769b15c105f4d118eab475993603
hunterxcobby <solomonsefah13@gmail.com>
2024-01-04 14:50:06 +0000

    error: C++ style comments are not allowed in ISO C90

--- 0x1D-Binary_trees/main.c
+++ 0x1D-Binary_trees/main.c
@@ -2,16 +2,17 @@
 
 int main(void) 
 {
-    node *root = NULL; // Initialize the family tree to be empty
+    node *root = NULL; /* Initialize the family tree to be empty*/
     
-    // Call the create function to build the family tree
+    /* Call the create function to build the family tree*/
     root = create();
 
 if (root != NULL) 
     {
         printf("Family Tree:\n");
         display(root, 0); /* Display the family tree */
-    } else 
+    }
+    else 
     {
         printf("Tree is empty.\n");
     }
