55b50a6c7ef5aa69361e9abb67243af8c1f0678d
hunterxcobby <solomonsefah13@gmail.com>
2024-01-04 14:28:57 +0000

    header file for implementing the binary tree

---
+++ 0x1D-Binary_trees/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node 
+{
+    int data;
+    node *left;
+    node *right;
+}node;
+
+node *create();
+node *display();
+node *free();
+
+#endif
