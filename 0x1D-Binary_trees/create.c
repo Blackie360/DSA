1bd7bee70e7972b0f54517b81e78c8e8c6d73009
hunterxcobby <solomonsefah13@gmail.com>
2024-01-04 14:28:27 +0000

    a function that creates a node in a binary tree

---
+++ 0x1D-Binary_trees/create.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/* Function to create a binary tree resembling a family tree */
+node *create() 
+{
+    int x;
+
+    /* Create a new family member, analogous to a new person in the family tree */
+    node *newnode = malloc(sizeof(node));
+
+    if (newnode == NULL) {
+        /* If adding a family member fails, indicate an issue in the family expansion */
+        printf("Error: Failed to add a new family member.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Ask for information about the family member to be added (-1 to stop adding) */
+    printf("Enter the details for the family member (-1 to stop adding): ");
+    scanf("%d", &x);
+
+    if (x == -1) {
+        free(newnode); /* If the decision is made to stop adding, remove the last family member */
+        return NULL;   /* Signal the end of family expansion */
+    }
+
+    /* Assign the details of the family member to the newly added member */
+    newnode->data = x;
+
+    /* Recursively add left and right family members, mirroring the expansion on each side */
+    printf("Enter details for the left family member of %d:\n", x);
+    newnode->left = create();
+
+    printf("Enter details for the right family member of %d:\n", x);
+    newnode->right = create();
+
+    return newnode; /* Return the fully expanded family tree */
+}
