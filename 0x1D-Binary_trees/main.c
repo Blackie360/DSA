3a3e2d73a22e3d526b5910636fdc2932630e7753
hunterxcobby <solomonsefah13@gmail.com>
2024-01-04 14:45:33 +0000

    the main program

---
+++ 0x1D-Binary_trees/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int main(void) 
+{
+    node *root = NULL; // Initialize the family tree to be empty
+    
+    // Call the create function to build the family tree
+    root = create();
+
+if (root != NULL) 
+    {
+        printf("Family Tree:\n");
+        display(root, 0); /* Display the family tree */
+    } else 
+    {
+        printf("Tree is empty.\n");
+    }
+
+    return 0;
+}
