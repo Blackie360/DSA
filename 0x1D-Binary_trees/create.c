a76976682f51d292f115796fd25b45be14bc3e68
hunterxcobby <solomonsefah13@gmail.com>
2024-01-05 18:08:35 +0000

    change printf statements

--- 0x1D-Binary_trees/create.c
+++ 0x1D-Binary_trees/create.c
@@ -8,17 +8,19 @@ node *create()
     /* Create a new family member, analogous to a new person in the family tree */
     node *newnode = malloc(sizeof(node));
 
-    if (newnode == NULL) {
+    if (newnode == NULL) 
+    {
         /* If adding a family member fails, indicate an issue in the family expansion */
         printf("Error: Failed to add a new family member.\n");
         exit(EXIT_FAILURE);
     }
 
     /* Ask for information about the family member to be added (-1 to stop adding) */
-    printf("Enter the details for the family member (-1 to stop adding): ");
+    printf("Enter data (-1 to stop): ");
     scanf("%d", &x);
 
-    if (x == -1) {
+    if (x == -1) 
+    {
         free(newnode); /* If the decision is made to stop adding, remove the last family member */
         return NULL;   /* Signal the end of family expansion */
     }
@@ -27,11 +29,11 @@ node *create()
     newnode->data = x;
 
     /* Recursively add left and right family members, mirroring the expansion on each side */
-    printf("Enter details for the left family member of %d:\n", x);
+    printf("Enter left child of %d:\n", x);
     newnode->left = create();
 
-    printf("Enter details for the right family member of %d:\n", x);
+    printf("Enter right child of %d:\n", x);
     newnode->right = create();
 
-    return newnode; /* Return the fully expanded family tree */
+    return (newnode); /* Return the fully expanded family tree */
 }
